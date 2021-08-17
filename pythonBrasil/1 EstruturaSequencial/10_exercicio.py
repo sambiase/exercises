d907e4e694cc1ef97b32bdcee9be1146eaf6d161
4n6ics <marsamb@gmail.com>
2021-08-17 11:23:37 -0300

    Added Exercise 10

---
+++ pythonBrasil/1 EstruturaSequencial/10_exercicio.py
@@ -0,0 +1,13 @@
+'''
+   Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+'''
+
+temp_celsius = float(input('Temperatura em Celsius --> '))
+fahrenheit = (temp_celsius * 1.8) + 32
+
+print(f'Temperatura em Fahrenheit --> {fahrenheit:.1f}')
+
+
+
+
