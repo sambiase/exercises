71af95f1a6fc82b1c5b42b7a098f487bc7e08fcb
4n6ics <marsamb@gmail.com>
2021-08-17 11:43:50 -0300

    Added Exercise 7

---
+++ pythonBrasil/1 EstruturaSequencial/7_exercicio.py
@@ -0,0 +1,14 @@
+'''
+   Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário
+'''
+
+area_quadrado = float(input('Medida do lado do quadrado --> '))
+total_area_quadrado = area_quadrado.__pow__(2)
+
+print(f'Area do Quadrado: {total_area_quadrado}')
+print(f'Dobro da Area do Quadrado: {2*total_area_quadrado}')
+
+
+
+
+
