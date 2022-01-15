d7c02ce28f5b51c0084bde9552f24a139cce35da
sambiase <martinho.sambiase@dremio.com>
2022-01-15 16:16:22 -0300

    added new file

---
+++ variousCodes/variavelGlobal.py
@@ -0,0 +1,16 @@
+'''
+    Variavel Global vs Variavel Local
+'''
+
+nome = 'Andre'
+
+
+def variavelLocal():
+    global nome
+    nome = 'Flavio'
+    return nome
+
+
+if __name__ == '__main__':
+    print(f'\nNome da Variavel Global: {nome}')
+    print(f'\nNome da Variavel Local: {variavelLocal()}')
