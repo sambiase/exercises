3c100a2b02b7f036e6ccfdc1dc3ef24756119311
sambiase <martinho.sambiase@dremio.com>
2022-03-01 14:22:55 -0300

    new files

--- variousCodes/asyncio/acyncio_example.py
+++ variousCodes/asyncio/acyncio_example.py
@@ -1,1 +1,12 @@
-def nome
+import asyncio
+
+
+async def main():
+    print('This is first line')
+    await asyncio.sleep(3)
+    print('This is second line')
+    await asyncio.sleep(3)
+
+
+res = main()
+asyncio.run(res)
