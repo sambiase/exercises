da54fd474c405bb9437668ac3c3e5e5ea855e9bc
sambiase <martinho.sambiase@dremio.com>
2022-03-02 16:03:38 -0300

    new files

--- variousCodes/sqlAlchemy/sqlAlchemy.py
+++ variousCodes/sqlAlchemy/sqlAlchemy.py
@@ -15,9 +15,13 @@ engine = sqlalchemy.create_engine('sqlite:///names.db', echo=True)  # this DB do
 # needs to import --> from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
+# CREATING A SESSION IN THE DB (from sqlalchemy.orm import sessionmaker)
+Session = sessionmaker(bind=engine)
+session = Session()
 
-class User(Base):  # class the creates users
 
+# CLASS TO CREATE USERS
+class User(Base):
     __tablename__ = 'users'  # table Name
     id = Column(Integer, primary_key=True)  # obrigatorio ter um PK
     name = Column(String(50))
@@ -26,25 +30,35 @@ class User(Base):  # class the creates users
 
 
 # TABLE CREATION IN THE DB
-Base.metadata.create_all(engine)  #its after this comand that the DB is created
+Base.metadata.create_all(engine)  # its after this command that the DB is actually created
 
 # CREATING USERS
-user = User(name='Andre',age=35,cidade='Rio de Janeiro')
+user = User(name='Andre', age=35, cidade='Rio de Janeiro')
 
-# CREATING A SESSION IN THE DB
-# from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# ADDING OBJECTS IN THE DB IS THE SAME AS "INSERT" FROM SQL
+# ADDING OBJECTS = "INSERT" DO SQL
 session.add(user)
+
 
 # NEEDS TO COMMIT IN ORDER TO PERSIST ON THE BD
 session.commit()
 
 # ADD MORE USERS
 session.add_all([
-    User(name='Jose',age=25,cidade='Rio de Janeiro'),
-    User(name='Flavia',age=45,cidade='Sao Paulo')])
+    User(name='Jose', age=25, cidade='Rio de Janeiro'),
+    User(name='Flavia', age=45, cidade='Sao Paulo')])
 
 session.commit()
+
+# CONSULTAR O DB = "SELECT" DO SQL
+res_one_user = session.query(User).filter_by(name='Jose')
+
+for all_users in session.query(User).order_by(User.id):
+    print(all_users.name)
+
+# CHANGE OBJECTS = "UPDATE" DO SQL
+user.name = 'Flamengo'
+session.commit()
+
+# DELETE AN OBJECT = "DELETE" NO SQL
+# user = session.query(User).filter_by(name='Flamengo')
+# session.delete(user)
