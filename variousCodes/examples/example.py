abd78bdc792786c83aed5908562dbde265d12af2
sambiase <martinho.sambiase@dremio.com>
2022-03-11 17:15:26 -0300

    new files

--- variousCodes/examples/example.py
+++ variousCodes/examples/example.py
@@ -4,14 +4,13 @@ strArr = ["[5, 2, 3]", "[2, 2, 3, 10, 6]"]
 resArr = []
 listResInt = []
 
-#remove spaces and brakets
-for i in range(0,len(strArr)):
+# remove spaces and brackets
+for i in range(0, len(strArr)):
     resArr.append(strArr[i].replace(']', '').replace('[', '').replace(' ', '').split(','))
 
-#map to Int
-for i in range(0,len(resArr)):
-    listResInt.append(list(map(int,resArr[i])))
-
+# map to Int
+for i in range(0, len(resArr)):
+    listResInt.append(list(map(int, resArr[i])))
 
 
 print(listResInt)
--- variousCodes/sqlAlchemy/sqlAlchemy.py
+++ variousCodes/sqlAlchemy/sqlAlchemy.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, session
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 
@@ -11,22 +11,30 @@ print(f'Versao do sqlAlchemy: {sqlalchemy.__version__}')
 # BD Connection - creation of Engine to connect to the BD
 engine = sqlalchemy.create_engine('sqlite:///names.db', echo=True)  # this DB does not exist ate the mo
 
-# Mapping the Object in Python to the one in the BD (ORM)
-# needs to import --> from sqlalchemy.orm import declarative_base
-Base = declarative_base()
 
 # CREATING A SESSION IN THE DB (from sqlalchemy.orm import sessionmaker)
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
-# CLASS TO CREATE USERS
+# MAPPING the Object in Python to the one in the BD (ORM)
+# needs to import --> from sqlalchemy.orm import declarative_base
+Base = declarative_base()
+
+
+# CLASS TO CREATE USERS and TO DEFINE TABLE PROPERTIES
+# ESTA CLASSE HERDA DE BASE QUE EH A CLASSE BASE DO SQLALCHEMY
 class User(Base):
-    __tablename__ = 'users'  # table Name
-    id = Column(Integer, primary_key=True)  # obrigatorio ter um PK
+    __tablename__ = 'users'  # TABLE NAME
+
+    id = Column(Integer, primary_key=True)  # obrigatório ter um PK
     name = Column(String(50))
     age = Column(Integer)
     cidade = Column(String(30))
+
+    # funcao usada para retornar os valores quando fizermos uma QUERY na tabela
+    def __repr__(self):
+        return f'Id: {self.id}, Nome: {self.name}, Idade: {self.age}, Cidade: {self.cidade}'
 
 
 # TABLE CREATION IN THE DB
@@ -38,20 +46,37 @@ user = User(name='Andre', age=35, cidade='Rio de Janeiro')
 # ADDING OBJECTS = "INSERT" DO SQL
 session.add(user)
 
-
 # NEEDS TO COMMIT IN ORDER TO PERSIST ON THE BD
 session.commit()
 
-# ADD MORE USERS
+# ADD MORE USERS AT ONCE
 session.add_all([
     User(name='Jose', age=25, cidade='Rio de Janeiro'),
     User(name='Flavia', age=45, cidade='Sao Paulo')])
 
-session.commit()
+# session.commit()
 
 # CONSULTAR O DB = "SELECT" DO SQL
-res_one_user = session.query(User).filter_by(name='Jose')
+res_one_user = session.query(User).filter_by(name='Andre')
+first_user = session.query(User).first()  # retorna o primeiro registro no BD
+getPrimaryKey = session.query(User).get(1)
+all_users = session.query(User.id,User.name).all()
+filter_ID = session.query(User).filter(User.id==2)
+user1 = session.query(User).filter(User.id==1).first()
+user2 = session.query(User).filter(User.id==1).first()
 
+print(f'{user1 is user2}')
+
+print(f'\nFirst user added in the DB: {first_user}')
+print(f'Primary Key ID 1: {getPrimaryKey}')
+print(f'All Users: {all_users}')
+print(f'Filtrar por Rio de Janeiro: {filter_ID}')
+
+
+
+
+session.close()
+'''
 for all_users in session.query(User).order_by(User.id):
     print(all_users.name)
 
@@ -62,3 +87,4 @@ session.commit()
 # DELETE AN OBJECT = "DELETE" NO SQL
 # user = session.query(User).filter_by(name='Flamengo')
 # session.delete(user)
+'''
