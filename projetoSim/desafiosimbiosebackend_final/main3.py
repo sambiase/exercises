ae3e22e92a135d2e400adc3d9953a3807099d9e7
sambiase <martinho.sambiase@dremio.com>
2022-03-21 16:47:18 -0300

    new files

---
+++ projetoSim/desafiosimbiosebackend_final/main3.py
@@ -0,0 +1,114 @@
+import json
+
+from flask import Flask, jsonify, request
+import pymysql
+import sqlalchemy
+from projetoSim.scratches import secrets
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String
+
+app = Flask(__name__)
+
+engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{secrets.password}@localhost/simChallenge', echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+# Table Creation on MySql
+class Team(Base):
+    __tablename__ = 'teams'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    team_name = Column(String(50))
+
+    def __repr__(self):
+        return f'ID: {self.id}, Team Name: {self.team_name}'
+
+
+# Table Creation on MySql
+class Recommendation(Base):
+    __tablename__ = 'recommendations'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    recommendations = Column(String(50))
+
+    def __repr__(self):
+        return f'ID: {self.id}, Recommendations: {self.recommendations}'
+
+
+Base.metadata.create_all(engine)
+
+
+@app.route('/teams', methods=['POST'])
+def register_teams():
+    # data gotten from POST BODY Json - Postman
+    request_data = request.get_json()
+
+    # check if team name exists
+    if "team_name" not in request_data:
+        return {"status": 400, "message": "ID and Team Name are mandatory fields"}
+    else:
+        teams = Team(id=request_data["id"], team_name=request_data["team_name"])
+
+        # INSERT data to MySql Table
+        session.add(teams)
+        session.commit()
+
+        # return posted on Postman
+        return jsonify(request_data)
+
+
+@app.route('/employees', methods=['POST'])
+def register_employees():
+    # data gotten from POST BODY Json - Postman
+    request_data = request.get_json()
+
+    return_dict = {"example": "Register employees!"}
+
+    return jsonify(return_dict)
+
+
+@app.route('/recommendations', methods=['POST'])
+def register_recommendations():
+    # data gotten from POST BODY Json - Postman
+    request_data = request.get_json()
+
+    if "recommendations" not in request_data:
+        return {"status": 400, "recommendations": "Recommendation is a mandatory field"}
+    else:
+        recommendations = Recommendation(id=request_data["id"], recommendations=request_data["recommendations"])
+
+        # INSERT data to MySql Table
+        session.add(recommendations)
+        session.commit()
+
+        # return on Postman
+        return jsonify(request_data)
+
+
+@app.route('/teams', methods=['GET'])
+def get_all_teams():
+    res = session.query(Team).all()
+
+    print(f'JSON RES: {res}')
+
+    return jsonify(res)
+
+
+@app.route('/recommendations', methods=['GET'])
+def get_all_recommendations():
+    res = session.query(Recommendation).all()
+
+    return jsonify(res)
+
+
+@app.route('/recommendations/employees', methods=['GET'])
+def get_all_employees_with_recommendations():
+    return_dict = {"example": "List employees!"}
+
+    return jsonify(return_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
