3310b1afe13a3d0e241eed59ec9fe0def7ce608d
sambiase <martinho.sambiase@dremio.com>
2022-04-19 16:20:36 -0300

    new files

---
+++ variousCodes/api/github_api.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuário no Github
+
+    :param usuario: str com o nome de usuário no github
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    print(resp.status_code)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    usuario = str(input('Digite o nome do usuario no GitHub: '))
+    res = buscar_avatar(usuario)
+    print(f'Usuario: {res}')
